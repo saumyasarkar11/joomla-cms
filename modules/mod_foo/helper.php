480fa59d1e5a213cfaed8200159730ecc41d7a57
Saumya Sarkar <saumyasarkar27@gmail.com>
2021-04-13 16:07:55 +0530

    Update helper.php

--- modules/mod_foo/helper.php
+++ modules/mod_foo/helper.php
@@ -1,5 +1,14 @@
 <?php
+/**
+ * @package    [PACKAGE_NAME]
+ *
+ * @author     [AUTHOR] <[AUTHOR_EMAIL]>
+ * @copyright  [COPYRIGHT]
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * @link       [AUTHOR_URL]
+ */
 
+// No direct access to this file
 defined('_JEXEC') or die;
 
 use Joomla\CMS\Session\Session;
